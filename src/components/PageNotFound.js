12670d4813f6e1ac23e1e79c1a61c1f9d231920f
Datng <nguyendat1411@gmail.com>
2020-01-27 14:22:09 +0700

    initial structure with components

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PageNotFound = () => <h1>Oops! Page Not Found</h1>;
+
+// export default PageNotFound;
---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class CoursesPage extends React.Component {
+  render() {
+    return <h2>Course Page</h2>;
+  }
+}
+
+export default CoursesPage;
